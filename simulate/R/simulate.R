f399177ef73946baa317ae25f542816f3b220852
lhertzog <7189850+lionel68@users.noreply.github.com>
2017-12-11 13:40:10 +0100

    first function commit

---
+++ simulate/R/simulate.R
@@ -0,0 +1,11 @@
+#' Simulate function
+#' 
+#' @author LH
+#' @description Simulate function
+#' @details The function simulate new response data
+#' 
+#' @export 
+#' @example inst/examples/simulateHelp.R
+simulate <- function(model){
+  #some stuff
+}
---
+++ simulate/R/simulate.gam.negbin.R
@@ -0,0 +1,1 @@
+#simulate.gam.negbin
