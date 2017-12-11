a02ce8081c88a5af42b6c57c5230bc7cf270552c
MiKatt <kattwinkel-mira@uni-landau.de>
2017-12-11 15:18:51 +0100

    documentation sim_xxx

--- simulate/R/sim_xxx.R
+++ simulate/R/sim_xxx.R
@@ -1,28 +1,47 @@
-#'sim_xxx
+#' sim_xxx
 #' @author Lionel Hertzog
+#' 
 #' @description Internal function to compute new prediction
-#' @details The function simulate new response data
+#' 
+#' @details Simulate new response data (so far for gam::mgcv)
+#' 
+#' @param nsim number of response vectors to simulate; defaults to 1
+#' @param pred predictions, generated with \code{predict(object, type = "link")}
+#' @param linkinv inverse link function.
+#' @param distr error distribution for the specific family
+#' @param rand.eff vector of random effects; defaults to NA = no random 
+#' effects
+#' @param cor.str matrix for the temporal or spatial correlation structure;
+#' defaults to NA = no correlation
 #' 
 #' @example inst/examples/simulate.gamHelp.R
 
 
-sim_xxx <- function(nsim,pred,linkinv,distr){
+sim_xxx <- function(nsim = 1, pred, linkinv, distr, rand.eff = NA, cor.str = NA ){
+  # TODO: option to keep draws from rand.eff and/or cor.str constant for all nsim
   
-  out <- matrix(NA,ncol=nsim,nrow=length(pred))
+  npred <- length(pred)
+  out <- matrix(NA, ncol = nsim, nrow = npred)
   
-  for(n in 1:nsim){
-    #add here before the random effect
-    sim_RE <- draw_RE()
-    #add here before the correlation structure
-    sim_CorStruct <- draw_CorStruct()
-    #get it all together
+  for(i in 1:nsim){
+    sim_RE <- rep(0, npred)
+    if(!is.na(rand.eff))
+    # draw random effects
+      sim_RE <- draw_RE()
+    
+    sim_CorStruct <- rep(0, npred)
+    if(!is.na(cor.str))
+    # draw from correlation structure
+      sim_CorStruct <- draw_CorStruct()
+    
+    # put everything together as the linear predictor
     linpred <- pred + sim_RE + sim_CorStruct
+    
     #inverse link the predicted values
     linpred_inv <- linkinv(linpred)
     
-    #draw nsim samples
-    out[,n] <- distr(linpred_inv)
+    # sample from the inverse linear predictor depending on the distribution
+    out[, i] <- distr(linpred_inv)
   }
-  
   return(out)
 }
