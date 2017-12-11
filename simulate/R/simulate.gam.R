32efa941c347bdc233511437a29127fab351b6d8
LoicChr <loic_chr@mailoo.org>
2017-12-11 15:58:30 +0100

    New families (negbin, beta, zero inflated pois)

--- simulate/R/simulate.gam.R
+++ simulate/R/simulate.gam.R
@@ -4,7 +4,7 @@
 #' @details The function simulate new response data
 #' 
 #' @export 
-#' @example inst/examples/simulate.gamHelp.R
+#' @example inst/examples/simulateGamHelp.R
 
 simulate.gam <- function(object,nsim){
   
@@ -14,18 +14,33 @@ simulate.gam <- function(object,nsim){
   fam <- object$family$family
   if(grep("Beta",fam)){
     distr <- function(linpred_inv){
-      rbeta
+      phi <- object$family$getTheta(trans =T)
+      rbeta(length(linpred_inv), shape1 = phi*linpred_inv, shape2 = phi-linpred_inv*phi) #Here linpred_inv is the mu of the beta distribution
     }
   }
   else if(grep("Negative binomial",fam)){
     distr <- function(linpred_inv){
-      rnbinom
+      rnbinom(length(linpred_inv), size = object$family$getTheta(), mu = linpred_inv)
     }
   }
   else if(grep("ziP",fam)){
     distr <- function(linpred_inv){
-      theta <- object$family$getTheta(trans=TRUE)
-      sim <- rzip(linpred_inv,theta)
+      theta <- object$family$getTheta(trans=FALSE)
+      sim <- function(linpred_inv,theta){
+        ## generate zero inflated Poisson random variables, where 
+        ## lambda = exp(linpred_inv), eta = theta[1] + exp(theta[2])*linpred_inv
+        ## and 1-p = exp(-exp(eta)).
+        y <- linpred_inv; n <- length(y)
+        lambda <- exp(linpred_inv)
+        eta <- theta[1] + exp(theta[2])*linpred_inv
+        p <- 1- exp(-exp(eta))
+        ind <- p > runif(n)
+        y[!ind] <- 0
+        np <- sum(ind)
+        ## generate from zero truncated Poisson, given presence...
+        y[ind] <- qpois(runif(np,dpois(0,lambda[ind]),1),lambda[ind])
+        y
+      } 
       return(sim)
     }
     
