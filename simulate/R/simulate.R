2e3b108fac73361b220624083b5e6f389cb085b5
MiKatt <kattwinkel-mira@uni-landau.de>
2017-12-11 14:01:39 +0100

    master fuction

--- simulate/R/simulate.R
+++ simulate/R/simulate.R
@@ -1,11 +1,16 @@
-#' Simulate function
+#' simulate new response
 #' 
 #' @author LH
-#' @description Simulate function
-#' @details The function simulate new response data
+#' @description Simulate one or more respones for gam::mgcv for family negbin, betar and ziP
+#' 
+#' @details Simulate new response data. 
+#' 
+#' @param object an object representing a fitted gam::mgcv model
+#' @param nsim number of response vectors to simulate. Defaults to 1
 #' 
 #' @export 
 #' @example inst/examples/simulateHelp.R
-simulate <- function(model){
+simulate <- function(objec, nsim = 1){
   #some stuff
+  simulate
 }
